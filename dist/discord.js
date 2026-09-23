4a72c9300d05ce006c58ac362f2582eac723b61f
alex <aldottexe@gmail.com>
2026-09-23 14:49:15 +0000

    fixed bug: thinking message not displaying

--- dist/discord.js
+++ dist/discord.js
@@ -1,22 +1,69 @@
-import process from 'node:process';
-import { URL } from 'node:url';
-import { Client, GatewayIntentBits } from 'discord.js';
-import { loadEvents } from './util/loaders.js';
-// Initialize the client
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-// Load the events and commands
-const events = await loadEvents(new URL('events/', import.meta.url));
-// Register the event handlers
-for (const event of events) {
-    client[event.once ? 'once' : 'on'](event.name, async (...args) => {
-        try {
-            await event.execute(...args);
-        }
-        catch (error) {
-            console.error(`Error executing event ${String(event.name)}:`, error);
-        }
-    });
+import { Client, DMChannel, Events, IntentsBitField, Partials, } from 'discord.js';
+import PQueue from 'p-queue';
+class DiscordChat {
+    #messageQueue = new PQueue({ concurrency: 1 });
+    #user;
+    #discord;
+    #commandCallbacks = {};
+    #messageCallbacks = [];
+    constructor(client, user) {
+        this.#user = user;
+        this.#discord = client;
+        // register command listener
+        this.#discord.on(Events.InteractionCreate, async (i) => {
+            if (i.isChatInputCommand() && i.commandName in this.#commandCallbacks)
+                this.#commandCallbacks[i.commandName](i);
+        });
+        // register message listener
+        this.#discord.on(Events.MessageCreate, (m) => {
+            if (!this.#validateReceivedMessage(m))
+                return;
+            this.#messageCallbacks.forEach((cb) => cb(m));
+        });
+    }
+    #validateReceivedMessage(m) {
+        if (m.author.bot)
+            return false;
+        if (m.author.id !== process.env.USER_ID)
+            return false;
+        const isDM = m.channel instanceof DMChannel;
+        if (!isDM)
+            return false;
+        return true;
+    }
+    async forwardMessageToUser(message, currentSession) {
+        return {
+            message: await this.#messageQueue.add(async () => this.#user.send(message)),
+            session: currentSession,
+        };
+    }
+    async registerCommand(commandInfo, command) {
+        if (commandInfo.name.length < 1)
+            throw new Error('Cannot register command of name length 0');
+        if (commandInfo.name in Object.keys(this.#commandCallbacks))
+            throw new Error(`Cannot re-register command of name ${commandInfo.name}`);
+        this.#discord.application?.commands.create(commandInfo);
+        this.#commandCallbacks[commandInfo.name] = command;
+    }
+    addOnMessageListener(onMessage) {
+        this.#messageCallbacks.push(onMessage);
+    }
 }
-// Login to the client
-void client.login(process.env.DISCORD_TOKEN);
+export async function createDiscordDMSession(userID, discordToken) {
+    const discord = new Client({
+        intents: [
+            IntentsBitField.Flags.Guilds,
+            IntentsBitField.Flags.GuildMessages,
+            IntentsBitField.Flags.MessageContent,
+            IntentsBitField.Flags.DirectMessages,
+        ],
+        partials: [Partials.Channel],
+    });
+    await discord.login(discordToken);
+    await new Promise((res) => discord.on('clientReady', res));
+    console.log(`✓ Bot is online as ${discord.user?.tag}`);
+    const user = await discord.users.fetch(userID);
+    console.log(`> Binding to dm: ${user.tag}`);
+    return new DiscordChat(discord, user);
+}
 //# sourceMappingURL=discord.js.map
--- src/index.ts
+++ src/index.ts
@@ -21,85 +21,83 @@ const dc = await createDiscordDMSession(process.env.USER_ID, process.env.DISCORD
 let currentSession: string;
 
 pi.addPiEventListener((e) => {
-	if (e.type !== 'message_end' || e.message.role !== 'assistant') return;
-	e.message.content.forEach((m) => {
-		if (m.type === 'text') {
-			dc.forwardMessageToUser(m.text, currentSession).then(({ message, session }) => {
-				if (message?.id) messageMap.mapMessage(session, message.id);
-			});
-		}
-	});
+   if (e.type !== 'message_end' || e.message.role !== 'assistant') return;
+   console.log(e.message.diagnostics);
+   e.message.content.forEach((m) => {
+      switch (m.type) {
+         case 'text':
+            dc.forwardMessageToUser(m.text, currentSession).then(({ message, session }) => {
+               if (message?.id) messageMap.mapMessage(session, message.id);
+            });
+            break;
+         case 'thinking':
+            dc.forwardMessageToUser(`-# 💭 ${thinkingResponse()}`, currentSession);
+            break;
+      }
+   });
 });
 
 pi.addPiEventListener((e) => {
-	if(e.type !== 'message_start' || e.message.role !== 'assistant') return;
-	e.message.content.forEach(m => {
-		if (m.type === 'thinking')
-			dc.forwardMessageToUser(`-# 💭 ${thinkingResponse()}`, currentSession);
-	})
-})
-
-pi.addPiEventListener((e) => {
-	if (e.type === 'tool_execution_start') {
-		dc.forwardMessageToUser(`-# 👾 ${e.toolName} ${JSON.stringify(e.args)}`, currentSession);
-	}
+   if (e.type === 'tool_execution_start') {
+      dc.forwardMessageToUser(`-# 👾 ${e.toolName} ${JSON.stringify(e.args)}`, currentSession);
+   }
 });
 
 function onError(e: string) {
-	dc.forwardMessageToUser(`-# ⚠️ ${e}`, currentSession);
+   dc.forwardMessageToUser(`-# ⚠️${e}`, currentSession);
 }
 pi.addOnErrorListener(onError);
 
 pi.addOnSessionChangeListener(({ name, path }) => {
-	if (path) currentSession = path;
-	dc.forwardMessageToUser(`-# Changed session: ${name ? name : path}`, path);
+   if (path) currentSession = path;
+   dc.forwardMessageToUser(`-# Changed session: ${name ? name : path}`, path);
 });
 
 dc.registerCommand(
-	{
-		name: 'new',
-		description: 'new session',
-	},
-	async (interaction) => {
-		if (interaction.commandName === 'new') currentSession = await pi.newSession();
-		interaction.reply(affirmativeResponse());
-	},
+   {
+      name: 'new',
+      description: 'new session',
+   },
+   async (interaction) => {
+      if (interaction.commandName === 'new') currentSession = await pi.newSession();
+      interaction.reply(affirmativeResponse());
+   },
 );
 
 dc.addOnMessageListener((m) => {
-	if (m.reference) return;
+   if (m.reference) return;
 
-	m.react('🧐');
-	const prompt = m.content;
+   m.react('🧐');
+   const prompt = m.content;
 
-	pi.queueAction({
-		event: 'message',
-		args: {
-			prompt: prompt,
-			sessionPath: currentSession,
-		},
-	});
+   pi.queueAction({
+      event: 'message',
+      args: {
+         prompt: prompt,
+         sessionPath: currentSession,
+      },
+   });
 });
 
 dc.addOnMessageListener(async (m) => {
-	if (!m.reference) return;
-	
-	m.react('↩️');
+   if (!m.reference) return;
 
-	const ref = await m.fetchReference();
-	if (!ref.content) return onError('The message you responded to is empty.');
-	if (!ref.id) return onError("Couldn't fetch message ID");
+   m.react('↩️');
 
-	const referencedSession = messageMap.selectSessionFromMessage(ref.id);
-	if (!referencedSession) return onError("The message you refrerenced to isn't tied to a session");
+   const ref = await m.fetchReference();
+   if (!ref.content) return onError('The message you responded to is empty.');
+   if (!ref.id) return onError("Couldn't fetch message ID");
 
-	const prompt = `in response to: "${ref.content}"\n\n${m.content}`;
+   const referencedSession = messageMap.selectSessionFromMessage(ref.id);
+   if (!referencedSession) return onError("The message you refrerenced to isn't tied to a session");
 
-	pi.queueAction({
-		event: 'message',
-		args: {
-			prompt: prompt,
-			sessionPath: referencedSession,
-		},
-	});
+   const prompt = `in response to: "${ref.content}"\n\n${m.content}`;
+
+   pi.queueAction({
+      event: 'message',
+      args: {
+         prompt: prompt,
+         sessionPath: referencedSession,
+      },
+   });
 });
